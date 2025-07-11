b9dccab9848dc39ce521dc4c02c04e7a8ede0435
marina-sheigets <sejmar1705@gmail.com>
2025-07-12 00:33:41 +0300

    feat: get year from params

--- src/components/YearInput/YearInput.tsx
+++ src/components/YearInput/YearInput.tsx
@@ -4,12 +4,15 @@ import { setUrlParam } from "../../utils/setURLParam";
 import { useAppDispatch } from "../../hooks/useAppDispatch";
 import { fetchMovies } from "../../redux/thunks/fetchMovies";
 import { parseFiltersFromURL } from "../../utils/parseFiltersFromURL";
+import { getURLParam } from "../../utils/getURLParam";
 
 function YearInput() {
   const dispatch = useAppDispatch();
   const currentYear = new Date().getFullYear();
 
-  const [selectedYear, setSelectedYear] = useState(currentYear.toString());
+  const initialYear = getURLParam("year") || currentYear.toString();
+
+  const [selectedYear, setSelectedYear] = useState(initialYear);
 
   const handleChangeYear = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedYear(e.target.value);
