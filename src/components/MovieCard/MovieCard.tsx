606b7996a61ace9c0faa853db2774370094a0be0
marina-sheigets <sejmar1705@gmail.com>
2025-07-11 15:26:19 +0300

    feat: add movie card component

---
+++ src/components/MovieCard/MovieCard.tsx
@@ -0,0 +1,23 @@
+import type { Movie } from '../../types/Movie'
+import MovieImage from '../MovieImage/MovieImage'
+import MovieInfo from '../MovieInfo/MovieInfo'
+import "./MovieCard.css";
+
+type MovieCardProps = {
+    movie: Movie
+}
+
+function MovieCard({ movie }: Readonly<MovieCardProps>) {
+  return (
+    <div className="movie-card">
+      <MovieImage
+        img={movie.img}
+        rating={movie.rating}
+        title={movie.title}
+      />
+      <MovieInfo movie={movie} />
+    </div>
+  )
+}
+
+export default MovieCard
