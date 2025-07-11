6744007bf63684eb1942496773cabb371f4391dd
marina-sheigets <sejmar1705@gmail.com>
2025-07-11 21:01:28 +0300

    feat: add not found image

--- src/components/MovieImage/MovieImage.tsx
+++ src/components/MovieImage/MovieImage.tsx
@@ -1,19 +1,26 @@
 import RatingChip from "../RatingChip/RatingChip";
 import "./MovieImage.css";
+import notFoundImage from '../../assets/image.png';
 
 type MovieImageProps = {
   img: string;
-    title: string;
-    rating: number;
+  title: string;
+  rating: number;
 };
 
 function MovieImage({ img, title, rating }: Readonly<MovieImageProps>) {
+  const imgURL = img === null ? notFoundImage : `https://image.tmdb.org/t/p/w500${img}`;
+  
   return (
     <div className="movie-image">
-      <img src={`https://image.tmdb.org/t/p/w500${img}`} alt={title} loading="lazy"/>
+      <img
+        src={imgURL}
+        alt={title}
+        loading="lazy"
+      />
       <RatingChip rating={rating} />
     </div>
-  )
+  );
 }
 
-export default MovieImage
+export default MovieImage;
