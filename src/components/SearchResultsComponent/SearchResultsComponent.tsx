0aeeaf996810690d6032fd5b843618f4df797cd8
marina-sheigets <sejmar1705@gmail.com>
2025-07-11 21:08:38 +0300

    fix: remove unnecessary props

--- src/components/SearchResultsComponent/SearchResultsComponent.tsx
+++ src/components/SearchResultsComponent/SearchResultsComponent.tsx
@@ -5,10 +5,9 @@ import SearchResultsHeader from "../SearchResultsHeader/SearchResultsHeader";
 import PaginationComponent from "../PaginationComponent/PaginationComponent";
 
 function SearchResultsComponent() {
-  const amountMoviesFound = 0; // This should be replaced with actual logic to get the number of movies found
   return (
     <SectionComponent>
-      <SearchResultsHeader amountMoviesFound={amountMoviesFound} />
+      <SearchResultsHeader />
       <PaginationComponent />
       <MoviesListComponent />
       <PaginationComponent />
