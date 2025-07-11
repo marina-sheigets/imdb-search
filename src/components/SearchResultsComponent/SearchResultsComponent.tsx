078ec495ceab786ae3d8d6645b3f3e4284caae57
marina-sheigets <sejmar1705@gmail.com>
2025-07-11 13:51:10 +0300

    feat: add search results header

--- src/components/SearchResultsComponent/SearchResultsComponent.tsx
+++ src/components/SearchResultsComponent/SearchResultsComponent.tsx
@@ -3,9 +3,13 @@ import './SearchResultsComponent.css'
 import SectionComponent from '../SectionComponent/SectionComponent'
 
 function SearchResultsComponent() {
+    const amountMoviesFound = 0; // This should be replaced with actual logic to get the number of movies found
   return (
     <SectionComponent>
+        <div className='search-results-header'>
         <Heading text="Search Results" variant='h2' color='secondary' align='left'/>
+        <Heading text={`${amountMoviesFound} movies found`} variant='h3' color='tertiary' align='right'/>
+        </div>
     </SectionComponent>
   )
 }
